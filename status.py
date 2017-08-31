f8f379962ed29f3e418acf935a938de779fda379
Pushkar Anand <anandpushkar088@gmail.com>
2017-09-01 00:45:55 +0530

    wrote menu items

--- status.py
+++ status.py
@@ -6,22 +6,22 @@
 #          Jono Bacon <jono@ubuntu.com>
 #          David Planella <david.planella@ubuntu.com>
 #
-# This program is free software: you can redistribute it and/or modify it 
+# This program is free software: you can redistribute it and/or modify it
 # under the terms of either or both of the following licenses:
 #
-# 1) the GNU Lesser General Public License version 3, as published by the 
+# 1) the GNU Lesser General Public License version 3, as published by the
 # Free Software Foundation; and/or
-# 2) the GNU Lesser General Public License version 2.1, as published by 
+# 2) the GNU Lesser General Public License version 2.1, as published by
 # the Free Software Foundation.
 #
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR 
-# PURPOSE.  See the applicable version of the GNU Lesser General Public 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the applicable version of the GNU Lesser General Public
 # License for more details.
 #
-# You should have received a copy of both the GNU Lesser General Public 
-# License version 3 and version 2.1 along with this program.  If not, see 
+# You should have received a copy of both the GNU Lesser General Public
+# License version 3 and version 2.1 along with this program.  If not, see
 # <http://www.gnu.org/licenses/>
 #
 import gi
@@ -31,35 +31,42 @@ from gi.repository import Gtk
 from gi.repository import AppIndicator3 as appindicator
 
 
-def menuitem_response(w, buf):
-  print buf
+def build_menu():
+    menu = Gtk.Menu()
+
+    item_start = Gtk.MenuItem('Start')
+    item_start.connect('activate', start)
+    menu.append(item_start)
+
+    item_stop = Gtk.MenuItem('Stop')
+    item_stop.connect('activate', stop)
+    menu.append(item_stop)
+
+    item_quit = Gtk.MenuItem('Quit')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+
+    menu.show_all()
+    return menu
+
+def start(_):
+    #start audio listener
+
+def stop(_):
+    #stop audio listener
+
+def quit(_):
+    Gtk.main_quit()
 
 if __name__ == "__main__":
   ind = appindicator.Indicator.new (
-                        "example-simple-client",
+                        "voice-assistant",
                         "indicator-messages",
                         appindicator.IndicatorCategory.APPLICATION_STATUS)
   ind.set_status (appindicator.IndicatorStatus.ACTIVE)
-  ind.set_attention_icon ("indicator-messages-new")
+  ind.set_attention_icon ("user-available")
 
-  # create a menu
-  menu = Gtk.Menu()
 
-  # create some 
-  for i in range(3):
-    buf = "Test-undermenu - %d" % i
-
-    menu_items = Gtk.MenuItem(buf)
-
-    menu.append(menu_items)
-
-    # this is where you would connect your menu item up with a function:
-    
-    # menu_items.connect("activate", menuitem_response, buf)
-
-    # show the items
-    menu_items.show()
-
-  ind.set_menu(menu)
+  ind.set_menu(build_menu())
 
   Gtk.main()
